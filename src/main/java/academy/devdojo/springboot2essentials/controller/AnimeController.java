c21eba3da35e6d71d4a45f7c8391de54e76c8b63
Andre <andreoliveira.adv@hotmail.com>
2020-12-05 15:32:41 -0500

    GET method 02 - release listAll, findById, ResponseEntity, HttpStatus, HttpStatusException, PathVariable, application.yml

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -5,8 +5,10 @@ import academy.devdojo.springboot2essentials.services.AnimeService;
 import academy.devdojo.springboot2essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +25,16 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id){
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
+
 
 }
